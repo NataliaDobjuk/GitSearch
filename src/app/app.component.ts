45bb4e677f5865e266068d2687a2342595068e67
NataliaDobjuk <nasyadobjuk@gmail.com>
2018-06-29 20:12:57 +0300

    Creating simple application for search git repositories

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GitSearchService } from './git-search.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  constructor(private GitSearchService: GitSearchService) {
+
+  }
+  ngOnInit() {
+   
+    //
+    /* this.GitSearchService.gitUsers('nataliia').then( (response) => {
+      alert("Nomber of users with name natalia: " + response.total_count);
+    }, (error) => {
+      alert("Error: " + error.statusText);
+    }) */
+
+  }
+  title = 'App is functional!';
 }
---
+++ src/app/git-search.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import { GitSearch } from './git-search';
+import { GitUsers } from './git-users';
+
+import { HttpClient } from '@angular/common/http';
+//import 'rxjs/add/operator/toPromise';
+//import 'rxjs/add/operators/toPromise';
+//import { toPromise } from 'rxjs/operators';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GitSearchService {
+  cachedValues: Array<{
+      [query: string]: GitSearch
+    }> = [];
+  cachedUsers : Array<{
+      [query: string]: GitUsers
+  }> = [];
+
+  constructor(private http: HttpClient) { }
+
+  gitSearch = (query: string): Promise<GitSearch> => {
+    let promise = new Promise<GitSearch>((resolve, reject) => {
+        if (this.cachedValues[query]) {
+            resolve(this.cachedValues[query])
+        }
+        else {
+            this.http.get('https://api.github.com/search/repositories?q=' + query)
+            .toPromise()
+            .then( (response) => {
+                resolve(response as GitSearch)
+            }, (error) => {
+                reject(error);
+            })
+        }
+    })
+    return promise;
+  }
+  gitUsers = (query: string): Promise<GitUsers> => {
+    let promise = new Promise<GitUsers>((resolve, reject) => {
+        if (this.cachedUsers[query]) {
+            resolve(this.cachedUsers[query])
+        }
+        else {
+            this.http.get('https://api.github.com/search/users?q=' + query + 'in:login')
+            .toPromise()
+            .then( (response) => {
+                resolve(response as GitUsers)
+            }, (error) => {
+                reject(error);
+            })
+        }
+    })
+    return promise;
+  }
+}
